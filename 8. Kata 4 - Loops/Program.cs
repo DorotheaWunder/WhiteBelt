c817b49add313d6759f79753e0508cc561e81680
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-11-04 12:31:29 +0100

    8. Kata 4 - Loops

---
+++ 8. Kata 4 - Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int totalWaves = 3;
+        int totalEnemies = 10;
+
+        for (int wave = 1; wave <= totalWaves; wave++)
+        {
+            Console.WriteLine($"Wave {wave} begins - prepare yourself!");
+            Thread.Sleep(500);
+            for (int enemy = 1; enemy <= totalEnemies; enemy++)
+            {
+                Console.WriteLine($"Enemy #{enemy} appears");
+                Thread.Sleep(300);
+            }
+            Console.WriteLine($"Wave {wave} completed - the next wave is approaching");
+            Console.WriteLine();
+            Thread.Sleep(1000);
+        }
+    }
+}
+
