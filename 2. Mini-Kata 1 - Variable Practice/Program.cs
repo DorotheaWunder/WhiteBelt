82491c88b7d32a4ce28d9c3964e16779ff8377e0
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-11-04 10:35:28 +0100

    White Belt practice tasks

---
+++ 2. Mini-Kata 1 - Variable Practice/Program.cs
@@ -0,0 +1,17 @@
+﻿void ProgramKata2()
+{
+    string playerName = "Hans-Peter";
+    int playerHealth = 100;
+    float playerAttack = 15.5f;
+    bool playerIsParalyzed = true;
+    double playerGold = 100.500;
+    
+    Console.WriteLine($"Player Name: {playerName}");
+    Console.WriteLine($"Health Points: {playerHealth}");
+    Console.WriteLine($"Attack Power: {playerAttack}");
+    Console.WriteLine($"Is Paralyzed: {playerIsParalyzed}");
+    Console.WriteLine($"Gold Coins: {playerGold}");
+}
+
+ProgramKata2();
+
---
+++ 3. Mini-Kata 2 - Variable Practice with Type Conversion/Program.cs
@@ -0,0 +1,20 @@
+﻿void ProgramKata3()
+{
+    string playerName = "Hans-Peter";
+    int playerHealth = 100;
+    float playerAttack = 15.5f;
+    //xp
+    bool playerIsParalyzed = true;
+    double playerGold = 100.500;
+    
+    
+    
+    
+    Console.WriteLine($"Player Name: {playerName}");
+    Console.WriteLine($"Health Points - as double - (implicit conversion): {playerHealth}");
+    Console.WriteLine($"Attack Power - as int - (explicit conversion): {playerAttack}");
+    Console.WriteLine($"Is Paralyzed: {playerIsParalyzed}");
+    Console.WriteLine($"Gold Coins: {playerGold}");
+}
+
+ProgramKata3();
