223a433f7c2343f22f51931abef4276a1fededad
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-11-04 11:50:22 +0100

    5. Kata 2 - Variables and Data Types

---
+++ 5. Kata 2 - Variables and Data Types/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string playerName = "Horst-Dieter";
+        int playerLevel = 5;
+        float playerHealth = 75.5f;
+        double playerExp = 1025.75;
+        bool playerHasMagic = true;
+        char playerRank = 'A';
+
+        double nextLvlTreshold = 10000.5000;
+        double toNextLvl = nextLvlTreshold - playerExp;
+        
+        
+        Console.WriteLine($"Character Name: {playerName}");
+        Console.WriteLine($"Level: {playerLevel}");
+        Console.WriteLine($"Health Points: {playerHealth}");
+        Console.WriteLine($"Experience Points: {playerExp}");
+        Console.WriteLine($"Has Magic Ability: {playerHasMagic}");
+        Console.WriteLine($"Rank: {playerRank}");
+        
+        Console.WriteLine($"Points needed to reach next level: {toNextLvl}");
+    }
+}
+
