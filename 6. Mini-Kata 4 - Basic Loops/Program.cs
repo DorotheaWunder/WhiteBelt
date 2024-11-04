b625be6d16eea6aac72c9e8a2ae10d7f90a794af
DorotheaWunder <dorothea.wunder@student.forsbergsskola.se>
2024-11-04 11:57:34 +0100

    6. Mini-Kata 4 - Basic Loops

---
+++ 6. Mini-Kata 4 - Basic Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int enemyNumbers = 10;
+        int enemiesSpawned = 0;
+
+        Console.WriteLine("The enemies spawn using a FOR-loop:");
+        for (int i = 0; i < enemyNumbers; i++)
+        {
+            Console.WriteLine($"Enemy #{i +1} has spawned");
+        }
+        
+        Console.WriteLine();
+        Console.WriteLine("The enemies spawn using a WHILE-loop:");
+        while (enemiesSpawned < enemyNumbers)
+        {
+            Console.WriteLine($"Enemy #{enemiesSpawned + 1} has spawned");
+            enemiesSpawned++;
+        }
+
+    }
+}
